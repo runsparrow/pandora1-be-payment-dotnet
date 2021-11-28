4f9735ce52bffa569df801c1f586f9afd9ecdfa3
Sheng.Xu <949525088@qq.com>
2021-11-28 15:09:34 +0800

    v1
    v1

--- PaymentAPI/Program.cs
+++ PaymentAPI/Program.cs
@@ -29,7 +29,7 @@ namespace PaymentAPI
                     {
                         options.Listen(IPAddress.Any, 8005, listenOptions =>
                         {
-                            listenOptions.UseHttps("fourlifecode-com-iis-0421170611.pfx", "fourlifecode");
+                            listenOptions.UseHttps("t-pic-cn-iis-1128142825.pfx", "t-pic.cn");
                         });
                     })
                     .UseStartup<Startup>()
